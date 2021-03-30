dcfb7e49272a1575945b3cb5f05788b7c59cc4e5
Jean-Edouard <Jean-Edouard@J-Edouard_PC>
2021-03-30 23:20:30 +0200

    J'ai supprimé le dao client et angel étant inutil

--- src/main/java/projetut/dao/ClientRepository.java
+++
@@ -1,8 +0,0 @@
-
-package projetut.dao;
-import org.springframework.data.jpa.repository.JpaRepository;
-import projetut.entity.Client;
-
-public interface ClientRepository extends JpaRepository<Client, Integer> {
-    
-}
