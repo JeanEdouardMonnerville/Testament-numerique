793aa171feddd0e6f340b08a4663ba52a08caebb
Jean-Edouard <Jean-Edouard@J-Edouard_PC>
2021-04-07 15:35:00 +0200

    Made with love

---
+++ src/main/resources/JS/OAuthLinkedIn.js
@@ -0,0 +1,7 @@
+const clientId="78mc6swcn9cr46"
+const secretClient="M6uZ3KA9XlsAYHOE"
+/*Il faut l'URL de redirection et le code l'URL EN ALLANT sur urlencoder.org
+*/
+
+const urlOauthLinkedIN=
+"https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=987654321&scope=scope&client_id="+clientID+"&redirect_uri=URLENCODE"
