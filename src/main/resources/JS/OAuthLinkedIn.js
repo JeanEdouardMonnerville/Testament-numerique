ae37b4e7dbf9458e79cce27ce96f571651e00f6b
Jean-Edouard <Jean-Edouard@J-Edouard_PC>
2021-04-07 17:26:34 +0200

    OAuthLinkedIn

--- src/main/resources/JS/OAuthLinkedIn.js
+++ src/main/resources/JS/OAuthLinkedIn.js
@@ -1,7 +1,13 @@
 const clientId="78mc6swcn9cr46"
 const secretClient="M6uZ3KA9XlsAYHOE"
-/*Il faut l'URL de redirection et le code l'URL EN ALLANT sur urlencoder.org
-*/
+urlEnCode="https%3A%2F%2Fprojettuteure81100.herokuapp.com%2F%0A"
 
 const urlOauthLinkedIN=
-"https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=987654321&scope=scope&client_id="+clientID+"&redirect_uri=URLENCODE"
+"https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=987654321&scope=scope&client_id="+clientId+"&redirect_uri="+urlEnCode
+
+instagram=document.getElementById("InstagramLogin");
+instagram.addEventListener("click",authentificationInstagrame);
+function authentificationInstagrame(event){
+    console.log(urlOauthLinkedIN);
+}
+
