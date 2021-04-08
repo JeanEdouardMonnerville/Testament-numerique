1f7dfba53236f7edde3b59cc7ff6800d8a6ee9bc
Jean-Edouard <Jean-Edouard@J-Edouard_PC>
2021-04-08 21:54:53 +0200

    J'AI REUSSI L'API LINKEDIN!!!!

--- src/main/resources/JS/OAuthLinkedIn.js
+++ src/main/resources/JS/OAuthLinkedIn.js
@@ -1,19 +1,26 @@
 const clientId="78mc6swcn9cr46"
 const secretClient="M6uZ3KA9XlsAYHOE"
-const urlEnCode="https%3A%2F%2Fprojettuteure81100.herokuapp.com%2F%0A"
-const url="https://projettuteure81100.herokuapp.com/"
+let scope="r_liteprofile";
+//Heroku
+const urlEnCode="https%3A%2F%2Fprojettuteure81100.herokuapp.com%2FIdentitfication%2Fsignin-linkedin%0A";
+const url="https://projettuteure81100.herokuapp.com/Identitfication/signin-linkedin";
 const urlOauthLinkedIN=
-"https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=31415926&scope=scope&client_id="+clientId+"&redirect_uri="+urlEnCode
+"https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=31415926&scope="+scope+"&client_id="+clientId+"&redirect_uri="+url;
+//LocalHost
+const urlEnCode1="http%3A%2F%2Flocalhost%3A8080%2FIdentitfication%0A";
+const url1="http://localhost:8080/Identitfication";
+const urlOauthLinkedIN1=
+"https://www.linkedin.com/oauth/v2/authorization?response_type=code&state=31415926&scope="+scope+"&client_id="+clientId+"&redirect_uri="+url1;
 
 
 
+LinkedIN=document.getElementById("LinkedInLogin");
 
-instagram=document.getElementById("LinkedInLogin");
-instagram.addEventListener("click",authentificationLinkedIN);
-function authentificationLinkedIN(event){
-    console.log(urlOauthLinkedIN);
+function authentificationLinkedIN(){
+   htmlref=urlOauthLinkedIN1;
+   LinkedIN.href=htmlref;
 }
-
+authentificationLinkedIN();
 
 
 
